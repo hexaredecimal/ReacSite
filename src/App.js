bb1f5b711f41502a4fc59f5740b17e3d65c307e0
Sibusiso Vincent <51314855+hexaredecimal@users.noreply.github.com>
2023-05-30 16:44:04 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import right_image from './pics.jpg';
+import './App.css';
+
+
+function Navigation() {
+  return (
+    <nav>
+      <div>
+        <h1>TECHBARGE</h1>
+      </div>
+      <div id="links">
+          <ul>
+            <li><a href="#">What We create</a></li>
+            <li><a href="#">Opportunities</a></li>
+            <li><a href="#">Indusry Examples</a></li>
+            <li><a href="#">Contact</a></li>
+            
+          </ul>
+      </div>
+    </nav>
+  )
+}
+
+function LeftContent() {
+  return (
+      <div>
+        <h1>Simplified <br/> Intergration</h1>
+        <p>
+          Let's import data from your existing systems like QuickBooks or 
+          even digitize your hard copy files. Add custom tags for easy
+          searchability later.
+        </p>
+      </div>
+  )
+}
+
+function RightContent() {
+  return (
+      <img src={right_image} />
+  )
+}
+
+function Container({id}, {contents}) {
+
+  if ({id}.id == "left") {
+    return (
+      <div id={id}>
+          <LeftContent/>
+      </div>
+    )
+  } else {
+    return (
+      <div id={id}>
+        <RightContent/>
+      </div>
+    )
+  }
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <Container id="left"/>
+      <Container id="normal"/>
+    </div>
+  );
+}
+
+export default App;
